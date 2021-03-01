f7187c15a729a82cff86c7827a37b7d8934503a4
Brittany Tinnin <tinfullstackdev@gmail.com>
2021-02-28 22:01:37 -0700

    removed comments

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,5 +1,3 @@
-// put all the panels in a NodeList, iterate through the NodeList and add an event listener. everytime you click on a panel, all active classes are removed from the panel and the panel you clicked on has the active class added to it
-
 let panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
